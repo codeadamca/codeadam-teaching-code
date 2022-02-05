898ac0f601c3313ac09282f6032fdb005d0dd0ec
Adam Thomas <thomasadam83@hotmail.com>
2022-02-05 08:28:17 -0500

    Revised css core

--- src/components/Twitter.js
+++ src/components/Twitter.js
@@ -5,9 +5,13 @@ import TweetEmbed from 'react-tweet-embed'
 class Twitter extends Component {
   render() {
     return (
-      <div className="Twitter">
+      <div className={"Twitter w3-padding-32 w3-" + (this.props.colour ? this.props.colour : "white")}>
         
-        <TweetEmbed id={this.props.id}></TweetEmbed>
+        <div className="ca-container-550 w3-center">
+
+          <TweetEmbed id={this.props.id}></TweetEmbed>
+
+        </div>
 
       </div>
     );
