050a883044158962f0599a7859b09807854a7248
Adam Thomas <thomasadam83@hotmail.com>
2022-07-31 23:29:17 -0400

    Added background to header

--- src/home/HomeHeader.js
+++ src/home/HomeHeader.js
@@ -17,11 +17,17 @@ class HomeHeader extends Component {
                 <div className="w3-cell w3-cell-middle w3-container w3-center">
                     
                 <h1 className="w3-text-white ca-font-medium ca-pt-sans">
+                  <span>
                   A Case Study of Pedagogical Approaches
                   <br />
                   Teaching Code in Postsecondary Education
+                  </span>
                 </h1>
-                <h2 className="w3-text-red ca-font-large ca-pt-sans">Adam Thomas | June 2020</h2>
+                <h2 className="w3-text-red ca-font-large ca-pt-sans">
+                  <span>
+                    Adam Thomas | June 2020
+                  </span>
+                </h2>
                 <p className="w3-text-white ca-font-small ca-pt-sans w3-center"></p>
                 </div>
             </div>
