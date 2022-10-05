ec6ddc8ba5c330d0f0c97cfb5e52429a64a06d94
Adam Thomas <thomasadam83@hotmail.com>
2022-10-04 22:12:55 -0400

    Update analytics

--- src/extensions/Page.js
+++ src/extensions/Page.js
@@ -2,13 +2,13 @@ import React, {useEffect} from 'react';
 import {Route} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import ReactGA from 'react-ga';
-ReactGA.initialize({trackingId: process.env.REACT_APP_TRACKING_ID});
+import ReactGA from "react-ga4";
+ReactGA.initialize(process.env.REACT_APP_TRACKING_ID);
 
 const Page = ({ title, ...rest }) => {
   useEffect(() => {
     document.title = (title ? title + " | " : "") + process.env.REACT_APP_TITLE;
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
   });
   return <Route {...rest} />;
 };
